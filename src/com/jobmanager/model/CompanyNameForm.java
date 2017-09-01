93868f2075af70c5aa3d2b8d532af37d49209ca2
ICE <358709544@qq.com>
2017-09-01 16:05:45 +0800

    Create CompanyNameForm.java

---
+++ src/com/jobmanager/model/CompanyNameForm.java
@@ -0,0 +1,26 @@
+package com.jobmanager.model;
+
+public class CompanyNameForm {
+	private String 	companyId;
+	private String 	companyShortName;
+	private String 	companyFeatures;
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyShortName() {
+		return companyShortName;
+	}
+	public void setCompanyShortName(String companyShortName) {
+		this.companyShortName = companyShortName;
+	}
+	public String getCompanyFeatures() {
+		return companyFeatures;
+	}
+	public void setCompanyFeatures(String companyFeatures) {
+		this.companyFeatures = companyFeatures;
+	}
+	
+}
