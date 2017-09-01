673747ed498d0d61f48df71a3633d6cec4653eaf
ICE <358709544@qq.com>
2017-09-01 15:54:14 +0800

    Create CompanyBaseInfoForm.java

---
+++ src/com/jobmanager/model/CompanyBaseInfoForm.java
@@ -0,0 +1,40 @@
+package com.jobmanager.model;
+
+public class CompanyBaseInfoForm {
+	private String companyId;
+	private String city;
+	private String industryField;
+	private String companySize;
+	private String companyUrl;
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getIndustryField() {
+		return industryField;
+	}
+	public void setIndustryField(String industryField) {
+		this.industryField = industryField;
+	}
+	public String getCompanySize() {
+		return companySize;
+	}
+	public void setCompanySize(String companySize) {
+		this.companySize = companySize;
+	}
+	public String getCompanyUrl() {
+		return companyUrl;
+	}
+	public void setCompanyUrl(String companyUrl) {
+		this.companyUrl = companyUrl;
+	}
+	
+}
