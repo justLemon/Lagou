1d2e6ad8084c34e43de4bd9c96ce9e0624c5d22e
ICE <358709544@qq.com>
2017-09-01 16:06:49 +0800

    Create Position.java

---
+++ src/com/jobmanager/model/Position.java
@@ -0,0 +1,111 @@
+package com.jobmanager.model;
+
+public class Position {
+	private String jobId;
+	private String companyId;
+	private String positionType;
+	private String positionName;
+	private String department;
+	private String jobNature;
+	private String salaryMin;
+	private String salaryMax;
+	private String workAddress;
+	private String workYear;
+	private String education;
+	private String positionAdvantage;
+	private String positionDetail;
+	private String positionAddress;
+	private String email;
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getPositionType() {
+		return positionType;
+	}
+	public void setPositionType(String positionType) {
+		this.positionType = positionType;
+	}
+	public String getPositionName() {
+		return positionName;
+	}
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getJobNature() {
+		return jobNature;
+	}
+	public void setJobNature(String jobNature) {
+		this.jobNature = jobNature;
+	}
+	public String getSalaryMin() {
+		return salaryMin;
+	}
+	public void setSalaryMin(String salaryMin) {
+		this.salaryMin = salaryMin;
+	}
+	public String getSalaryMax() {
+		return salaryMax;
+	}
+	public void setSalaryMax(String salaryMax) {
+		this.salaryMax = salaryMax;
+	}
+	public String getWorkAddress() {
+		return workAddress;
+	}
+	public void setWorkAddress(String workAddress) {
+		this.workAddress = workAddress;
+	}
+	public String getWorkYear() {
+		return workYear;
+	}
+	public void setWorkYear(String workYear) {
+		this.workYear = workYear;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getPositionAdvantage() {
+		return positionAdvantage;
+	}
+	public void setPositionAdvantage(String positionAdvantage) {
+		this.positionAdvantage = positionAdvantage;
+	}
+	public String getPositionDetail() {
+		return positionDetail;
+	}
+	public void setPositionDetail(String positionDetail) {
+		this.positionDetail = positionDetail;
+	}
+	public String getPositionAddress() {
+		return positionAddress;
+	}
+	public void setPositionAddress(String positionAddress) {
+		this.positionAddress = positionAddress;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
