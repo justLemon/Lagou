90656f54b21241a0e5c524c72ab35c1bcb8c22c6
ICE <358709544@qq.com>
2017-09-01 15:53:45 +0800

    Create Company.java

---
+++ src/com/jobmanager/model/Company.java
@@ -0,0 +1,103 @@
+package com.jobmanager.model;
+
+public class Company {
+	private String userName;
+	private String companyId;
+	private String companyShortName;
+	private String companyFeatures;
+	private String labels;
+	private String city;
+	private String industryField;
+	private String companySize;
+	private String companyUrl;
+	private String product;
+	private String productUrl;
+	private String productProfile;
+	private String productPicUrl;
+	public String getProductPicUrl() {
+		return productPicUrl;
+	}
+	public void setProductPicUrl(String productPicUrl) {
+		this.productPicUrl = productPicUrl;
+	}
+	private String companyProfile;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyShortName() {
+		return companyShortName;
+	}
+	public void setCompanyShortName(String companyShortName) {
+		this.companyShortName = companyShortName;
+	}
+	public String getCompanyFeatures() {
+		return companyFeatures;
+	}
+	public void setCompanyFeatures(String companyFeatures) {
+		this.companyFeatures = companyFeatures;
+	}
+	public String getLabels() {
+		return labels;
+	}
+	public void setLabels(String labels) {
+		this.labels = labels;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getIndustryField() {
+		return industryField;
+	}
+	public void setIndustryField(String industryField) {
+		this.industryField = industryField;
+	}
+	public String getCompanySize() {
+		return companySize;
+	}
+	public void setCompanySize(String companySize) {
+		this.companySize = companySize;
+	}
+	public String getCompanyUrl() {
+		return companyUrl;
+	}
+	public void setCompanyUrl(String companyUrl) {
+		this.companyUrl = companyUrl;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getProductUrl() {
+		return productUrl;
+	}
+	public void setProductUrl(String productUrl) {
+		this.productUrl = productUrl;
+	}
+	public String getProductProfile() {
+		return productProfile;
+	}
+	public void setProductProfile(String productProfile) {
+		this.productProfile = productProfile;
+	}
+	public String getCompanyProfile() {
+		return companyProfile;
+	}
+	public void setCompanyProfile(String companyProfile) {
+		this.companyProfile = companyProfile;
+	}
+    
+}
