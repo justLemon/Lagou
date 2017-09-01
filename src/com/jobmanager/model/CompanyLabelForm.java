63e526b842f0af25f37f3bf2648ca3bbc598a76d
ICE <358709544@qq.com>
2017-09-01 16:05:23 +0800

    Create CompanyLabelForm.java

---
+++ src/com/jobmanager/model/CompanyLabelForm.java
@@ -0,0 +1,19 @@
+package com.jobmanager.model;
+
+public class CompanyLabelForm {
+	private String companyId;
+	private String labels;
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getLabels() {
+		return labels;
+	}
+	public void setLabels(String labels) {
+		this.labels = labels;
+	}
+	
+}
