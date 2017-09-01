9660240e65b3ed244d70cc3545d779dd47015a98
ICE <358709544@qq.com>
2017-09-01 16:06:06 +0800

    Create CompanyProductForm.java

---
+++ src/com/jobmanager/model/CompanyProductForm.java
@@ -0,0 +1,40 @@
+package com.jobmanager.model;
+
+public class CompanyProductForm {
+	private String companyId;
+	private String productPicUrl;
+	private String product;
+	private String productUrl;
+	private String productProfile;
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getProductPicUrl() {
+		return productPicUrl;
+	}
+	public void setProductPicUrl(String productPicUrl) {
+		this.productPicUrl = productPicUrl;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getProductUrl() {
+		return productUrl;
+	}
+	public void setProductUrl(String productUrl) {
+		this.productUrl = productUrl;
+	}
+	public String getProductProfile() {
+		return productProfile;
+	}
+	public void setProductProfile(String productProfile) {
+		this.productProfile = productProfile;
+	}
+	
+}
