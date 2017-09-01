b95189723567ada464cfb1d114866163c150abf3
ICE <358709544@qq.com>
2017-09-01 16:06:24 +0800

    Create CompanyProfileForm.java

---
+++ src/com/jobmanager/model/CompanyProfileForm.java
@@ -0,0 +1,18 @@
+package com.jobmanager.model;
+
+public class CompanyProfileForm {
+	private String 	companyId;
+	private String 	companyProfile;
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyProfile() {
+		return companyProfile;
+	}
+	public void setCompanyProfile(String companyProfile) {
+		this.companyProfile = companyProfile;
+	}
+}
